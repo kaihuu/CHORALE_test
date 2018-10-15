591850c7dd1cea1775450e773bc2b86f7f3d661e
kaihuu <uemura-toshiaki-xh@ynu.jp>
2018-10-15 18:30:41 +0900

    add heatmapGenerateProcess

--- heatmap.py
+++ heatmap.py
@@ -7,40 +7,58 @@ from DBAccessor import DBAccessor as dbac
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
 def sturgesFormula(n):
     return round(1 + math.log2(n))
 
+def generateBinData(data):
+    #ビン数計算
+    Num_of_bin = sturgesFormula(len(data))
+    npresult = np.array(data, dtype='float')
+    
+    max = np.amax(npresult, axis=0)
+    min = np.amin(npresult, axis=0)
+    
+    l = (max - min) / Num_of_bin
+
+    xax = generateAxisData(Num_of_bin, min[0], l[0])
+    yax = generateAxisData(Num_of_bin, min[1], l[1])
+
+    return xax, yax, npresult
+
+def generateAxisData(Num_of_bin, min, length):
+    ax = np.array([])
+    for i in range(Num_of_bin):
+        ax = np.append(ax, [min + float(i) * length])
+    return ax
+
+def generateHeatmapData(Num_of_bin, np, xax, yax):
+    df_result = pd.DataFrame(np)
+    for i in range(Num_of_bin):
+        for t in range(Num_of_bin):
+            df_af = df_result[(df_result[0] >= xax[i]) & (df_result[0] < xax[i]) 
+            & (df_result[1] >= yax[t]) & (df_result[1] < yax[t])]
+            len(df_af)
+
+    return result
+
+
 np.random.seed(0)
 
 #クエリ実行
 result = dbac.ExecuteQueryFromList(dbac.QueryString(), [306])
-#ビン数計算
-Num_of_bin = sturgesFormula(len(result))
 
-npresult = np.array(result, dtype='float')
+xax, yax, npresult = generateBinData(result)
 
-print(npresult)
+df_result = pd.DataFrame(npresult)
 
-max = np.amax(npresult, axis=0)
-min = np.amin(npresult, axis=0)
+df_af = df_result[(df_result[0] >= xax[3]) & (df_result[0] < xax[4]) & (df_result[1] >= yax[3]) & (df_result[1] < yax[4])]
+print(df_af)
+print(len(df_af))
 
-print(max)
-print(min)
-print(Num_of_bin)
-
-l = (max - min) / Num_of_bin
-print(l)
-
-xax = np.array([])
-for i in range(Num_of_bin):
-    print(min[0] + float(i) * l[0])
-    xax = np.append(xax, [min[0] + float(i) * l[0]])
-
-
-print(xax)
 
 uniform_data = np.random.rand(100, 100)
 #sns.heatmap(npresult)
