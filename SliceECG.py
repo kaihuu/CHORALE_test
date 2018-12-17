30a54dbc80b72e9937c76fda7b96b42e693336bf
kaihuu <uemura-toshiaki-xh@ynu.jp>
2018-12-17 15:15:27 +0900

    modify

--- SliceECG.py
+++ SliceECG.py
@@ -3,9 +3,22 @@ import plotly.graph_objs as go
 import plotly.offline as offline
 from DBAccessor import DBAccessor as dbac
 import numpy as np
+from datetime import datetime,timedelta
+import os
+import googlemaps
 
-def getTrace(testID, name):
+def getTrace(semanticLinkID):
     #result = dbac.ExecuteQueryFromList(dbac.QueryStringGetLatencyTestTime(), [testID])
+    endpoints = dbac.ExecuteQueryFromList(dbac.QueryStringGetEndPoints(), [semanticLinkID])
+    
+    now= datetime.now()
+
+    matrix = getMatrix(now, str(endpoints[1][2]), str(endpoints[1][3]),
+     str(endpoints[0][2]), str(endpoints[0][3]))
+    
+    duration = matrix["rows"][0]["elements"][0]["duration_in_traffic"]["value"]
+    print(duration)
+    print(type(duration))
     yresult = np.array([1.0,1.11,1.21,1.1,1.2])
     xresult = np.full(5,"Convert Loss")
 
@@ -19,8 +32,24 @@ def getTrace(testID, name):
     )
     return trace0
 
+def getMatrix(now, startLatitude, startLongitude, endLatitude, endLongitude):
+    matrix_result = gmaps.distance_matrix((startLatitude, startLongitude),
+                                        (endLatitude, endLongitude),
+                                        mode="driving",
+                                        traffic_model = "best_guess",
+                                        departure_time=now
+                                        )
 
-data = [getTrace(85, "N=1")]
+    return matrix_result
+
+
+#API前準備
+api_key = os.environ["GOOGLE_MAP_API_KEY"]
+gmaps = googlemaps.Client(key=api_key)
+
+
+
+data = [getTrace(207)]
 
 layout = go.Layout(
     xaxis=dict(tickfont=dict(size=20)),
